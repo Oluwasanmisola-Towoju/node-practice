16b621db7501046bef6fe784aafbf57ea9e071af
Oluwasanmisola Towoju <oluwasanmisolatowoju@gmail.com>
2026-04-16 17:51:53 +0100

    Using express for http operations

---
+++ express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+//app.get
+//app.post
+//app.put
+//app.all
+//app.use
+//app.listen
+//app.delete
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found</h1>');
+});
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+});
