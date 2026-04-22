14fbff2d57d3350352d756d9f5064dc7361c0dce
Oluwasanmisola Towoju <oluwasanmisolatowoju@gmail.com>
2026-04-22 19:09:31 +0100

    Add logout functionality to authentication controller and routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -97,5 +97,17 @@ const login = async (req, res) => {
     });
 };
 
+const logout = async (req, res) => {
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res
+    .status(200)
+    .json({
+        status: "success",
+        message: "Loggedout successfully"
+    });
+};
 
-export { register, login };
+export { register, login, logout };
