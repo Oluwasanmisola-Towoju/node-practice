71ff9e639fdb8b50cfd753641577e821a8350a29
Oluwasanmisola Towoju <oluwasanmisolatowoju@gmail.com>
2026-04-23 14:24:37 +0100

    Add removeFromWatchlist functionality with ownership verification

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -56,4 +56,43 @@ const addToWatchlist = async (req, res) => {
     });
 };
 
-export { addToWatchlist };
+const removeFromWatchlist = async (req, res) => {
+    // Find watchlistItem and verify ownership
+    const watchlistItem = await prisma.watchlistItem.findUnique({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    if (!watchlistItem) {
+        return res.status(404)
+        .json({
+            error: "Watchlist item not found"
+        });
+    }
+
+    // Ensure only owner can delete 
+    if (watchlistItem.userId !== req.user.id) {
+        return res
+        .status(403)
+        .json({
+            error: "Not allowed to update this watchlist item"
+        });
+    }
+
+    // Delete watchlist Item
+    await prisma.watchlistItem.delete({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res
+    .status(200)
+    .json({
+        status: "success",
+        message: "Movie removed from watchlist successfully"
+    });
+}
+
+export { addToWatchlist, removeFromWatchlist };
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addToWatchlist } from "../controllers/watchlistController.js";
+import { addToWatchlist, removeFromWatchlist } from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.post('/', addToWatchlist);
+
+router.delete('/:id', removeFromWatchlist);
 
 export default router;
